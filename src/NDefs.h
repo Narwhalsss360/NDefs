119fb4ccd1c29769d4a55dfd3321adb36e332760
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-06-07 22:34:50 -0400

    Indent

--- src/NDefs.h
+++ src/NDefs.h
@@ -18,20 +18,20 @@
 #define NULLTERMINATOR '\0'
 
 #ifdef ARDUINO_ARCH_AVR
-#define ULONG_MAX 4294967295
-#define LONG_MAX 2147483647
-#define LONG_MIN -2147483648
+    #define ULONG_MAX 4294967295
+    #define LONG_MAX 2147483647
+    #define LONG_MIN -2147483648
 
-#define UINT_MAX 65535
-#define INT_MAX 32767
-#define INT_MIN -32768
+    #define UINT_MAX 65535
+    #define INT_MAX 32767
+    #define INT_MIN -32768
 
-#define BYTE_MAX 255
+    #define BYTE_MAX 255
 
-#define ADC_MAX 1023
-#define PWN_MAX 255
+    #define ADC_MAX 1023
+    #define PWN_MAX 255
 
-#define AVR_RESET() ((void (*)(void))0x00)()
+    #define AVR_RESET() ((void (*)(void))0x00)()
 #endif // ARDUINO_ARCH_AVR
 
 #define SERIALCOM_BAUD 1000000
