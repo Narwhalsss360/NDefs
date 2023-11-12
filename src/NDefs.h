287e6facbbeecfda07963bbcc569ca94b98bb721
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-11-12 17:09:28 -0500

    Removed unnecessary macros

--- src/NDefs.h
+++ src/NDefs.h
@@ -7,16 +7,6 @@
 #define NDEFS_PATCH 0
 
 #ifdef ARDUINO_ARCH_AVR
-    #define ULONG_MAX 4294967295
-    #define LONG_MAX 2147483647
-    #define LONG_MIN -2147483648
-
-    #define UINT_MAX 65535
-    #define INT_MAX 32767
-    #define INT_MIN -32768
-
-    #define BYTE_MAX 255
-
     #define ADC_MAX 1023
     #define PWN_MAX 255
 
@@ -24,7 +14,7 @@
 #endif
 
 #define reinterpret_c_style(type, origin) *((type *)origin)
-#define isBetween(l, v, h) (l < v && v < h) 
+#define isBetween(l, v, h) (l < v && v < h)
 #define isBetweenOr(l, v, h) (l <= v && v <= h)
 #define boolToString(v) (v ? "True" : "False")
 
